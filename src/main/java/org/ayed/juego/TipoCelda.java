0dd09feffa06fed7e4bfc329819f2971e463b3dd
marianaseltzer <mseltzer@fi.uba.ar>
2026-06-20 18:05:07 -0300

    Enhance TipoCelda enum with comments
    Added comments to enum values for clarity.

--- src/main/java/org/ayed/juego/TipoCelda.java
+++ src/main/java/org/ayed/juego/TipoCelda.java
@@ -1,9 +1,14 @@
 package org.ayed.juego;
 
+// Cada elemento del enum queda por defecto asignado con un valor ordinal
+// Si yo quisiera saber de que se trata 
+// TipoCelda celda = TipoCelda.INCENDIADA;
+// System.out.println(celda.name()); -> Imprime: "INCENDIADA"
+// System.out.println(celda.name()); -> Imprime: 2
 public enum TipoCelda {
-    NORMAL,
-    PARED,
-    INCENDIADA,
-    CONGELADA,
-    ELECTRIFICADA
+    NORMAL, // 0
+    PARED, // 1
+    INCENDIADA, // 2
+    CONGELADA, // 3
+    ELECTRIFICADA // 4
 }
