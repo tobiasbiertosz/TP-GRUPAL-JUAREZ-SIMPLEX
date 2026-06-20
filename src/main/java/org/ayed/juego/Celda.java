e62d67addf7fc18234eaac2e60910a1c1c052164
marianaseltzer <mseltzer@fi.uba.ar>
2026-06-20 18:06:33 -0300

    Enhance Celda class with item and entity management
    Added item and entity attributes with corresponding getters and setters. Implemented methods for item collection and entity removal.

--- src/main/java/org/ayed/juego/Celda.java
+++ src/main/java/org/ayed/juego/Celda.java
@@ -1,24 +1,72 @@
 package org.ayed.juego;
 
+import org.ayed.poe.*;
+
 public class Celda {
 
     private Posicion posicion;
-    private TipoCelda tipo;
+    private TipoCelda tipo; // Incendida - Normal - Congelada - Pared - Electrificada
 
+    private Item item; // armas
+    private Entidad entidad; // Jugador - Monstruo
+
+    // PRE: Reciba una posicion y un tipo de celda
+    // POS: -
     public Celda(Posicion posicion, TipoCelda tipo) {
         this.posicion = posicion;
         this.tipo = tipo;
     }
 
     public Posicion getPosicion() {
-        return posicion;
+        return this.posicion;
     }
 
     public TipoCelda getTipo() {
-        return tipo;
+        return this.tipo;
+    }
+
+    public Item getItem() {
+        return this.item;
+    }
+
+    public Entidad getEntidad() {
+        return this.entidad;
     }
 
     public void setTipo(TipoCelda tipo) {
         this.tipo = tipo;
     }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public void setEntidad(Entidad entidad) {
+        this.entidad = entidad;
+    }
+
+    // PRE: -
+    // POS: Devuelve true si la celda es transitable (no es pared), false en caso
+    // contrario
+    public boolean esTransitable() {
+        return tipo != TipoCelda.PARED;
+    }
+
+    // PRE: -
+    // POS: Devuelve el item que se encuentra en la celda y lo elimina de la misma
+    public Item recogerItem() {
+
+        Item aux = item;
+
+        item = null;
+
+        return aux;
+    }
+
+    // PRE: -
+    // POS: Elimina la entidad que se encuentra en la celda
+    public void eliminarEntidad() {
+        this.entidad = null;
+    }
+
 }
