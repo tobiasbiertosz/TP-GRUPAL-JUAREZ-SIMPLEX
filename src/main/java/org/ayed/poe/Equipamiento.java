6d50eddd79de37aea8f7fe2c24a7a583ec25c9ed
Smaidana <smaidana@fi.uba.ar>
2026-06-20 13:42:29 -0300

    Update Equipamiento.java

--- src/main/java/org/ayed/poe/Equipamiento.java
+++ src/main/java/org/ayed/poe/Equipamiento.java
@@ -20,7 +20,9 @@ public class Equipamiento {
 
 	public Equipamiento() {
 	}
-
+	
+//Getters
+	
 	public Item getArmadura() {
 		return armadura;
 	}
@@ -56,40 +58,246 @@ public class Equipamiento {
 	public Item getArmaPrincipal() {
 		return armaPrincipal;
 	}
-
+	
+//Equipar
 	public void equiparArmadura(Item item) {
-		armadura = item;
+		if (item != null && item.getTipo() != TipoItem.ARMADURA) {
+			throw new IllegalArgumentException();
+		}
+
+		this.armadura = item;
 	}
 
 	public void equiparBotas(Item item) {
-		botas = item;
+		if (item != null && item.getTipo() != TipoItem.BOTAS) {
+			throw new IllegalArgumentException();
+		}
+
+		this.botas = item;
 	}
 
 	public void equiparGuantes(Item item) {
-		guantes = item;
+		if (item != null && item.getTipo() != TipoItem.GUANTES) {
+			throw new IllegalArgumentException();
+		}
+
+		this.guantes = item;
 	}
 
 	public void equiparCasco(Item item) {
-		casco = item;
+		if (item != null && item.getTipo() != TipoItem.CASCO) {
+			throw new IllegalArgumentException();
+		}
+
+		this.casco = item;
 	}
 
 	public void equiparCinturon(Item item) {
-		cinturon = item;
+		if (item != null && item.getTipo() != TipoItem.CINTURON) {
+			throw new IllegalArgumentException();
+		}
+
+		this.cinturon = item;
 	}
 
 	public void equiparAnillo1(Item item) {
-		anillo1 = item;
+		if (item != null && item.getTipo() != TipoItem.ANILLO) {
+			throw new IllegalArgumentException();
+		}
+
+		this.anillo1 = item;
 	}
 
 	public void equiparAnillo2(Item item) {
-		anillo2 = item;
+		if (item != null && item.getTipo() != TipoItem.ANILLO) {
+			throw new IllegalArgumentException();
+		}
+
+		this.anillo2 = item;
 	}
 
 	public void equiparPendiente(Item item) {
-		pendiente = item;
+		if (item != null && item.getTipo() != TipoItem.PENDIENTE) {
+			throw new IllegalArgumentException();
+		}
+
+		this.pendiente = item;
 	}
 
 	public void equiparArma(Item item) {
-		armaPrincipal = item;
+		if (item != null && item.getTipo() != TipoItem.ARMA) {
+			throw new IllegalArgumentException();
+		}
+
+		this.armaPrincipal = item;
 	}
+
+	//Desequipar
+	public Item desequiparArmadura() {
+		Item item = armadura;
+		armadura = null;
+		return item;
+	}
+
+	public Item desequiparBotas() {
+		Item item = botas;
+		botas = null;
+		return item;
+	}
+
+	public Item desequiparGuantes() {
+		Item item = guantes;
+		guantes = null;
+		return item;
+	}
+
+	public Item desequiparCasco() {
+		Item item = casco;
+		casco = null;
+		return item;
+	}
+
+	public Item desequiparCinturon() {
+		Item item = cinturon;
+		cinturon = null;
+		return item;
+	}
+
+	public Item desequiparAnillo1() {
+		Item item = anillo1;
+		anillo1 = null;
+		return item;
+	}
+
+	public Item desequiparAnillo2() {
+		Item item = anillo2;
+		anillo2 = null;
+		return item;
+	}
+
+	public Item desequiparPendiente() {
+		Item item = pendiente;
+		pendiente = null;
+		return item;
+	}
+
+	public Item desequiparArma() {
+		Item item = armaPrincipal;
+		armaPrincipal = null;
+		return item;
+	}
+
+	//Estadisticas totales
+	public int getVidaTotal() {
+
+		return obtenerVida(armadura)
+				+ obtenerVida(botas)
+				+ obtenerVida(guantes)
+				+ obtenerVida(casco)
+				+ obtenerVida(cinturon)
+				+ obtenerVida(anillo1)
+				+ obtenerVida(anillo2)
+				+ obtenerVida(pendiente)
+				+ obtenerVida(armaPrincipal);
+	}
+
+	public int getManaTotal() {
+
+		return obtenerMana(armadura)
+				+ obtenerMana(botas)
+				+ obtenerMana(guantes)
+				+ obtenerMana(casco)
+				+ obtenerMana(cinturon)
+				+ obtenerMana(anillo1)
+				+ obtenerMana(anillo2)
+				+ obtenerMana(pendiente)
+				+ obtenerMana(armaPrincipal);
+	}
+
+	public int getDanoAtaqueTotal() {
+
+		return obtenerDanoAtaque(armadura)
+				+ obtenerDanoAtaque(botas)
+				+ obtenerDanoAtaque(guantes)
+				+ obtenerDanoAtaque(casco)
+				+ obtenerDanoAtaque(cinturon)
+				+ obtenerDanoAtaque(anillo1)
+				+ obtenerDanoAtaque(anillo2)
+				+ obtenerDanoAtaque(pendiente)
+				+ obtenerDanoAtaque(armaPrincipal);
+	}
+
+	public int getDanoHechizoTotal() {
+
+		return obtenerDanoHechizo(armadura)
+				+ obtenerDanoHechizo(botas)
+				+ obtenerDanoHechizo(guantes)
+				+ obtenerDanoHechizo(casco)
+				+ obtenerDanoHechizo(cinturon)
+				+ obtenerDanoHechizo(anillo1)
+				+ obtenerDanoHechizo(anillo2)
+				+ obtenerDanoHechizo(pendiente)
+				+ obtenerDanoHechizo(armaPrincipal);
+	}
+
+	public int getArmaduraTotal() {
+
+		return obtenerArmadura(armadura)
+				+ obtenerArmadura(botas)
+				+ obtenerArmadura(guantes)
+				+ obtenerArmadura(casco)
+				+ obtenerArmadura(cinturon)
+				+ obtenerArmadura(anillo1)
+				+ obtenerArmadura(anillo2)
+				+ obtenerArmadura(pendiente)
+				+ obtenerArmadura(armaPrincipal);
+	}
+
+	//Auxiliares
+	private int obtenerVida(Item item) {
+
+		if (item == null) {
+			return 0;
+		}
+
+		return item.getVida();
+	}
+
+	private int obtenerMana(Item item) {
+
+		if (item == null) {
+			return 0;
+		}
+
+		return item.getMana();
+	}
+
+	private int obtenerDanoAtaque(Item item) {
+
+		if (item == null) {
+			return 0;
+		}
+
+		return item.getDanoAtaque();
+	}
+
+	private int obtenerDanoHechizo(Item item) {
+
+		if (item == null) {
+			return 0;
+		}
+
+		return item.getDanoHechizo();
+	}
+
+	private int obtenerArmadura(Item item) {
+
+		if (item == null) {
+			return 0;
+		}
+
+		return item.getArmadura();
+	}
+	
+	
 }
