1a80007313220f8ff951e8b63bbf806e2b2dfb43
Lcalahuana-cloud <lcalahuana@fi.uba.ar>
2026-06-22 01:56:37 -0300

    quito el test de main
    Removed unused code and simplified main method.

--- src/main/java/org/ayed/Main.java
+++ src/main/java/org/ayed/Main.java
@@ -17,66 +17,6 @@ import java.util.List;
 public class Main {
 	public static void main(String[] args) {
 		
-		// ----------------------------
-        // MAPA (GRAFO SIMPLE)
-        // ----------------------------
-        Grafo<Posicion> grafo = new Grafo<>();
-
-        Posicion a = new Posicion(0, 0);
-        Posicion b = new Posicion(0, 1);
-        Posicion c = new Posicion(0, 2);
-        Posicion d = new Posicion(1, 0);
-        Posicion e = new Posicion(1, 1);
-
-        grafo.agregarVertice(a);
-        grafo.agregarVertice(b);
-        grafo.agregarVertice(c);
-        grafo.agregarVertice(d);
-        grafo.agregarVertice(e);
-
-        grafo.agregarArista(a, b, 1);
-        grafo.agregarArista(b, c, 1);
-        grafo.agregarArista(a, d, 2);
-        grafo.agregarArista(d, e, 1);
-        grafo.agregarArista(e, c, 1);
-
-        // ----------------------------
-        // ENTIDADES
-        // ----------------------------
-        Mazmorra mazmorra = new Mazmorra(3, 3);
-
-        Monstruo monstruo = new Monstruo("Goblin", 50, 10, a);
-
-        Personaje jugador = new Personaje("Heroe", 100, 15, c);
-
-        // ----------------------------
-        // IA
-        // ----------------------------
-        ControladorMonstruo ia = new ControladorMonstruo();
-
-        // ----------------------------
-        // SIMULACION (TICKS)
-        // ----------------------------
-        for (int i = 0; i < 5; i++) {
-
-            System.out.println("\n--- TICK " + i + " ---");
-
-            ia.actualizar(monstruo, jugador, mazmorra, grafo);
-
-            System.out.println("Monstruo en: "
-                    + monstruo.getPosicion().getFila()
-                    + ", "
-                    + monstruo.getPosicion().getColumna()
-            );
-
-            System.out.println("Estado: " + ia.getEstado());
-
-            // mover jugador para ver comportamiento dinámico (opcional)
-            if (i == 2) {
-                jugador = new Personaje("Heroe", 100, 15, a);
-            }
-        }
-
 		Personaje heroe = new Personaje("Guerrero", 100, 20, new Posicion(0, 0));
 
 		Monstruo goblin = new Monstruo("Goblin", 50, 10, new Posicion(1, 1));
